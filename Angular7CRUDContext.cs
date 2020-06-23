8031b1618b3169976f6c4b1cff6fc214aec7e346
Jacob Ajao <jacob.ajao@fcmb.com>
2020-06-23 17:10:26 +0100

    Initial Commit

---
+++ Angular7CRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using Angular7CRUDAPI.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular7CRUDAPI
+{
+    public class Angular7CRUDContext: DbContext
+    {
+        public Angular7CRUDContext(DbContextOptions<Angular7CRUDContext> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserId> UserId { get; set; }
+
+    }
+}
